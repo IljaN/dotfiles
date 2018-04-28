0f5ac0f6c93942f9d36d733ee703119091476a8b
Ilja Neumann <ineumann@owncloud.com>
2018-04-28 04:47:20 +0200

    . i3 lenovo config - fish config - urxvt config

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,26 @@
+function fish_prompt --description 'Write out the prompt'
+    set -l color_cwd
+    set -l suffix
+    
+    switch "$USER"
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+        case '*'
+            set color_cwd $fish_color_cwd
+            set suffix 'λ'
+    end
+
+    switch (prompt_pwd)
+	case '~' 
+	    set separ ''
+	case '*'
+	    set separ ':'
+    end
+    echo -n -s (set_color -d $color_cwd) (prompt_pwd) (set_color 165118)"$separ"(set_color  white)"$suffix "(set_color normal)
+    #echo -n -s (set_color -d $color_cwd) (set_color --bold $color_cwd)"$suffix "(set_color normal)
+end
